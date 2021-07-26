3b904dfbda3909dfb78b5c1eb64a7bd44f15b75b
Jveen-D <d85066965@icloud.com>
2021-07-26 19:17:59 +0800

    myself template init.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path')
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+      // 'vue': 'vue/dist/vue.esm-bundler.js',//配置template
+    }
+  }
+})
