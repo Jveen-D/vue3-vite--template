7bf6372548e6fbf9d43f397438cbd8b2111b6a7e
Jveen-D <d85066965@icloud.com>
2022-01-07 14:06:32 +0800

    chore:添加element-plus并配置国际化

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
  * @FilePath: /vue3-vite--template/src/App.vue
 -->
 <template>
-    <suspense>
-      <router-view />
-    </suspense>
+  <el-config-provider :locale="zhCn"> </el-config-provider>
+  <suspense>
+    <router-view />
+  </suspense>
 </template>
 <script lang="ts" setup>
+  import { ElConfigProvider } from 'element-plus';
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+
   import { tailwindTheme } from './utils/tailwind/tailwindTheme';
   const { changeTheme } = tailwindTheme();
   changeTheme(localStorage.theme);
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -4,9 +4,16 @@
  * @FilePath: /vue3-vite--template/src/pages/home/home.vue
 -->
 <template>
-  <div class="w-full h-full p-8"> home </div>
+  <div class="w-full h-full p-8"> 
+    <el-button>ok</el-button>
+    <el-date-picker v-model="value1" type="date" placeholder="Pick a day">
+      </el-date-picker>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {ref} from 'vue'
+const value1 = ref('')
+</script>
 
 <style lang="scss" scoped></style>
